473e1f5cdff6694ccdf4f3b4cbd2ae38aa273b66
Diego Assis <diegoalex.assis@gmail.com>
2024-04-06 15:40:56 -0300

    ajuste create table

--- Model/Objetos_Banco/SPR_DEL_TIPOS_PASSOS.sql
+++ Model/Objetos_Banco/SPR_DEL_TIPOS_PASSOS.sql
@@ -1,19 +1,15 @@
-CREATE OR REPLACE FUNCTION "db_DxCorp".spr_del_tipos_passos(
-    id_tipo_passo_param INT DEFAULT NULL,
-    ds_tipo_passo_param VARCHAR(100) DEFAULT NULL
-)
-RETURNS void 
-LANGUAGE plpgsql
-AS $$
+CREATE OR REPLACE PROCEDURE "db_DxCorp".spr_del_tipos_passos(
+	IN id_tipo_passo_param integer 
+	)
+LANGUAGE 'plpgsql'
+AS $BODY$
 BEGIN
     IF id_tipo_passo_param IS NOT NULL THEN
         DELETE FROM "db_DxCorp"."tblTipos_Passo"
         WHERE ID_Tipo_Passo = id_tipo_passo_param;
-    ELSIF ds_tipo_passo_param IS NOT NULL THEN
-        DELETE FROM "db_DxCorp"."tblTipos_Passo"
-        WHERE Ds_Tipo_Passo = ds_tipo_passo_param;
     ELSE
-        RAISE EXCEPTION 'É necessário fornecer um ID ou uma descrição para exclusão.';
+        RAISE EXCEPTION 'Nenhum parâmetro fornecido para exclusão.';
     END IF;
 END;
-$$;
+$BODY$;
+
