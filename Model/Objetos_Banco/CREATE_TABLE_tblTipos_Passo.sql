76d55244c2869dd7c1cbc0b97f29282080b5bf77
Diego Assis <diegoalex.assis@gmail.com>
2024-04-06 14:59:48 -0300

    ajuste create table

--- Model/Objetos_Banco/CREATE_TABLE_tblTipos_Passo.sql
+++ Model/Objetos_Banco/CREATE_TABLE_tblTipos_Passo.sql
@@ -2,14 +2,9 @@
 
 -- DROP TABLE IF EXISTS "db_DxCorp"."tblTipos_Passo";
 
-CREATE TABLE IF NOT EXISTS "db_DxCorp"."tblTipos_Passo"
-(
-    id_tipo_passo integer NOT NULL DEFAULT nextval('"db_DxCorp"."tblTipos_Passo_id_tipo_passo_seq"'::regclass),
-    ds_tipo_passo character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "tblTipos_Passo_pkey" PRIMARY KEY (id_tipo_passo)
-)
+CREATE TABLE IF NOT EXISTS "db_DxCorp"."tblTipos_Passo" (
+    id_tipo_passo SERIAL PRIMARY KEY,
+    ds_tipo_passo VARCHAR(100) NOT NULL
+);
 
-TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS "db_DxCorp"."tblTipos_Passo"
-    OWNER to "diego.assis";
